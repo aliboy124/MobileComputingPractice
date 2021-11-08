ca2e0a2a36eb559a45024123a87e4b6c61612a77
aliboy124 <aligreat124@gmail.com>
2021-11-08 19:00:06 +0500

    Intent Activity *calling feature using intent

--- app/src/main/java/com/example/practiceapplication/MainActivity.java
+++ app/src/main/java/com/example/practiceapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.practiceapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void CallingIntent(View view){
+        Uri uri = Uri.parse("tel:+923351076620");
+        Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(intent);
     }
 }
