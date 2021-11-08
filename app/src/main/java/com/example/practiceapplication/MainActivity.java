aac6c521d2abd1b0121c6c19d0d85a7d1970d285
aliboy124 <aligreat124@gmail.com>
2021-11-08 19:30:38 +0500

    Intent Activity *share chooser implemented

--- app/src/main/java/com/example/practiceapplication/MainActivity.java
+++ app/src/main/java/com/example/practiceapplication/MainActivity.java
@@ -16,9 +16,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void CallingIntent(View view){
-        Uri uri = Uri.parse("tel:+923351076620");
-        Intent intent = new Intent(Intent.ACTION_DIAL,uri);
-        startActivity(intent);
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "Here is the share content body";
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
     }
 
     public void NewActivity(View view){
