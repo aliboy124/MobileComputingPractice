b9413d77108e8a45cdaf548e08c5713b706ff403
aliboy124 <aligreat124@gmail.com>
2021-11-08 19:09:26 +0500

    Intent Activity *sent data from one activity to another succesfully

--- app/src/main/java/com/example/practiceapplication/MainActivity.java
+++ app/src/main/java/com/example/practiceapplication/MainActivity.java
@@ -20,4 +20,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_DIAL,uri);
         startActivity(intent);
     }
+
+    public void NewActivity(View view){
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        intent.putExtra("sample text","HELOOO THIS IS SENT FROM MAIN ACITIVTY");
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/practiceapplication/MainActivity2.java
+++ app/src/main/java/com/example/practiceapplication/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.practiceapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        TextView txt = (TextView) findViewById(R.id.textView2);
+        txt.setText(intent.getStringExtra("sample text"));
     }
 }
