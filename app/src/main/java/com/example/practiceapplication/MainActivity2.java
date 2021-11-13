8523afe09ae131abb116218ba12d08167ec9b868
aliboy124 <aligreat124@gmail.com>
2021-11-14 02:03:04 +0500

    Practice Dialogues with different views as well

--- app/src/main/java/com/example/practiceapplication/MainActivity2.java
+++ app/src/main/java/com/example/practiceapplication/MainActivity2.java
@@ -1,9 +1,14 @@
 package com.example.practiceapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -15,5 +20,17 @@ public class MainActivity2 extends AppCompatActivity {
         Intent intent = getIntent();
         TextView txt = (TextView) findViewById(R.id.textView2);
         txt.setText(intent.getStringExtra("sample text"));
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this);
+        //builder.setMessage("MA MESSAGE");
+        //builder.setTitle("MA TITLE");
+        builder.setCancelable(true);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View myView = layoutInflater.inflate(R.layout.my_layout,(ViewGroup)findViewById(R.id.toastBase) );
+        builder.setView(myView);
+        //builder.setIcon(R.drawable.ic_baseline_account_balance_24);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
