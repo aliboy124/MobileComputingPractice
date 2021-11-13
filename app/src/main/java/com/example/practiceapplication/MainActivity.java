c342fe385ac15e4cbe470336e0f01efa9f06222a
aliboy124 <aligreat124@gmail.com>
2021-11-14 01:39:08 +0500

    Created a custom toast with different view

--- app/src/main/java/com/example/practiceapplication/MainActivity.java
+++ app/src/main/java/com/example/practiceapplication/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View myView = layoutInflater.inflate(R.layout.my_layout,(ViewGroup)findViewById(R.id.toastBase) );
+        Toast toast = new Toast(getApplicationContext());
+        toast.setView(myView);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.show();
     }
 
     public void CallingIntent(View view){
